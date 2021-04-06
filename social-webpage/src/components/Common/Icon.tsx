5cc20d6e99a328e057a82cf6623e69c3d006e9e9
Tarzanica <patrycja.bobek@microsoft.wsei.edu.pl>
2021-04-06 13:45:11 +0200

    created Icon component

---
+++ social-webpage/src/components/Common/Icon.tsx
@@ -0,0 +1,12 @@
+import React, {FC} from 'react';
+import styled from 'styled-components'; 
+
+const StyledImage = styled.img`
+    padding: 5px;
+`;
+
+const Icon = (props:any) => (
+    <StyledImage src={props.src} alt="" />
+);
+
+export default Icon;
